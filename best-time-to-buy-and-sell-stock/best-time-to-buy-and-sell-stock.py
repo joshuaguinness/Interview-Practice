09d805db3526500853c1d5c57202d233f4dbaf20
Joshua Guinnness <joshua.guinness@gmail.com>
2021-11-20 12:26:55 -0500

    Time: 1112 ms (43.45%), Space: 25.2 MB (11.61%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # diff = [0]*len(prices)
+        if not prices: return 0
+        
         max_profit = 0
         min_purchase = prices[0]
         
         for i, price in enumerate(prices):
             max_profit = max(max_profit, price - min_purchase)
             min_purchase = min(min_purchase, price)
-            
-            # max_gain = 0
-            # for j in range(i+1, len(prices)):
-            #     if (max_gain < (prices[j] - price)):
-            #         max_gain = (prices[j] - price)
-            # diff[i] = max_gain
              
         return max_profit
-        
-        
-        
