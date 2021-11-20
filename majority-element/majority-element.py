fa1fa7595993bd2160f3f6e117d60ce49004a16c
Joshua Guinnness <joshua.guinness@gmail.com>
2021-11-20 14:05:27 -0500

    Time: 164 ms (78.08%), Space: 15.7 MB (11.85%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,6 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        # Cheap Way
-        c = Counter(nums)
-        return (c.most_common(1)[0][0])
+        # Using Boyer-Moore Majority Vote Algorithm
+        
+        count = 1
+        major = nums[0]
+
+        for i in range(1,len(nums)):
+            if (nums[i] != major):
+                count -= 1
+                if (count == 0):
+                    major = nums[i]
+                    count += 1
+            else:
+                count += 1
+                
+        return major
+        
+        # Quick and Dirty Way
+
+        # c = Counter(nums)
+        # return (c.most_common(1)[0][0])
