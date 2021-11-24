15ab6952b5b4932a977c187a46c5d22a08fa76c9
Joshua Guinnness <joshua.guinness@gmail.com>
2021-11-23 21:38:35 -0500

    Time: 51 ms (15.21%), Space: 14.6 MB (45.00%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,21 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        
-        values = []
-        self.recursiveFunction(root, values, 0)
-        return values
     
-    def recursiveFunction(self, node: Optional[TreeNode], values, level):
+    # DFS Method
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
         
-        if node is None:
-            return
+#         values = []
+#         self.recursiveFunction(root, values, 0)
+#         return values
+    
+#     def recursiveFunction(self, node: Optional[TreeNode], values, level):
         
-        if len(values) <= level:
-            values.append([])
-        values[level].append(node.val)
-        self.recursiveFunction(node.left, values, level+1)
-        self.recursiveFunction(node.right, values, level+1)
+#         if node is None:
+#             return
+        
+#         if len(values) <= level:
+#             values.append([])
+#         values[level].append(node.val)
+#         self.recursiveFunction(node.left, values, level+1)
+#         self.recursiveFunction(node.right, values, level+1)
+        
+     # BFS Method   
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        queue = deque([root])
+        values = []       
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            values.append(level)
+        return values
