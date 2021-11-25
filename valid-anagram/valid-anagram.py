06657cef83f0355254990b21a7b32da592fbbc16
Joshua Guinnness <joshua.guinness@gmail.com>
2021-11-24 21:46:57 -0500

    Time: 52 ms (55.26%), Space: 14.6 MB (32.12%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,11 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter1 = Counter(s)
-        counter2 = Counter(t)
         
-        # counter1.sort()
-        # counter2.sort()
-        
-        return counter1 == counter2
-        
-        
+        # Easy method using counter        
+        # return Counter(s) == Counter(t)
+    
+        # Using default dictionary
+        dict1 = defaultdict(int)
+        dict2 = defaultdict(int)
+        for i, v in enumerate(s):
+            dict1[v] += 1
+        for i, v in enumerate(t):
+            dict2[v] += 1
+            
+        return sorted(dict1.items()) == sorted(dict2.items())
