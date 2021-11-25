de6df76294b93cf93b6293a0f2359aeb4ed7cc29
Joshua Guinnness <joshua.guinness@gmail.com>
2021-11-25 08:54:39 -0500

    Time: 38 ms (23.79%), Space: 14.3 MB (62.32%) - LeetHub

--- binary-tree-paths/binary-tree-paths.py
+++ binary-tree-paths/binary-tree-paths.py
@@ -9,10 +9,44 @@ class Solution:
         
         # Using List Comprehension
         
-        if not root: 
+        # if not root: 
+        #     return []
+        # if root.right == None and root.left == None:
+        #     return [str(root.val)]
+        # result = [str(root.val) + '->' + i for i in self.binaryTreePaths(root.left)]
+        # result += [str(root.val) + '->' + i for i in self.binaryTreePaths(root.right)]
+        # return result
+        
+        
+        # Recursive DFS
+#         if not root:
+#             return []
+        
+#         result = []
+#         self.dfs(root, result, "")
+#         return result
+#     def dfs(self, root, result, string):
+#         if root.left == None and root.right == None:
+#             result.append(string + str(root.val))
+#             return
+#         if root.left != None:
+#             self.dfs(root.left, result, string + str(root.val) + "->")
+#         if root.right != None:
+#             self.dfs(root.right, result, string + str(root.val) + "->")
+        
+        # BFS w/ Queue
+        if not root:
             return []
-        if root.right == None and root.left == None:
-            return [str(root.val)]
-        result = [str(root.val) + '->' + i for i in self.binaryTreePaths(root.left)]
-        result += [str(root.val) + '->' + i for i in self.binaryTreePaths(root.right)]
+        
+        result = []
+        queue =  collections.deque([(root, "")])
+        while queue:
+            node, string = queue.popleft()
+            if node.left == None and node.right == None:
+                result.append(string + str(node.val))
+            if node.left:
+                queue.append((node.left, string + str(node.val) + "->"))
+            if node.right:
+                queue.append((node.right, string + str(node.val) + "->"))
+        
         return result
