27212fb4a1113cc7e504b01e89ff52e5d7caaee8
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-03 22:39:55 -0500

    Time: 120 ms (67.73%), Space: 16.4 MB (23.33%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -5,6 +5,8 @@ class Solution:
         to_remove = set()
         new_string = []
         
+        # First pass, add all '(' to set, pop when get ')'
+        # If get ')' with empty set, add index to set to remove
         for i, char in enumerate(s):
             if char == '(':
                 stack.append(i)
@@ -14,16 +16,16 @@ class Solution:
                 else:
                     to_remove.add(i)
                     
+        # Union of stack and set
+        # Add all extra '(' in the stack to the set
         to_remove = to_remove.union(set(stack))
             
+        # Second pass
         # Remove all extra '(' from the string
         for i, char in enumerate(s):
             if i not in to_remove:
                 new_string.append(char)
                 
+        # Create a new string
         return "".join(new_string)
-            
-        
-            
-            
             
