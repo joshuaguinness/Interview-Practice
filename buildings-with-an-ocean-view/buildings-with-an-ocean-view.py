e8c4c63d8035f9bbde1db2603787418f8430cab8
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-04 16:06:07 -0500

    Time: 676 ms (86.61%), Space: 30.9 MB (95.02%) - LeetHub

---
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        
+        length_heights = len(heights)
+        max_height = heights[-1]
+        buildings_set = {length_heights-1}        
+        
+        for i in range(2,len(heights)+1):
+            if heights[-i] > max_height:
+                buildings_set.add(length_heights-i)
+                max_height = heights[-i]
+                
+            
+        return sorted(buildings_set)
