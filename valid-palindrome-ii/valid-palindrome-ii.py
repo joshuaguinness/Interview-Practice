f6c413090fd296259112c2fd343d4f6669cf68cf
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-05 10:34:21 -0500

    Time: 108 ms (73.73%), Space: 14.6 MB (72.75%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.py
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,14 +1,6 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-        # One pointer from left to right
-        # Another pointer from right to left
-        # compare them, if same okay
-        # if not same, add one to keep tracking
-        # if need to add more than 1, return false
-        # if both are at same index and keep track <= 1 return true
-        
-        keep_track = 0
         i = 0
         j = len(s) - 1
         
@@ -18,9 +10,12 @@ class Solution:
             char2 = s[j]
             
             if (char1 != char2):
+                # Split remaining string into two splices
+                # includes i and excludes j
                 string_pos_1 = s[i:j]
+                # includes j and excludes i
                 string_pos_2 = s[i+1:j+1]
-                print(string_pos_1, string_pos_2)
+                # if one of these is a valid palindrome, then return True
                 return string_pos_1 == string_pos_1[::-1] or string_pos_2 == string_pos_2[::-1]
             
             i += 1
