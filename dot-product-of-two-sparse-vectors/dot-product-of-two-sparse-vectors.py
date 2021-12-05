3b1c1a30521f5b3fd8b53f2ae15f91689d261ff4
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-05 14:07:44 -0500

    Time: 1700 ms (99.05%), Space: 18.2 MB (87.11%) - LeetHub

--- dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,4 +1,5 @@
 class SparseVector:
+    # Approach 1: Array of vectors, brute force but faster approach
     def __init__(self, nums: List[int]):
         self.nums = nums
         
@@ -8,7 +9,35 @@ class SparseVector:
         for i, v in enumerate(self.nums):
             product += v*vec.nums[i]
         return product
-        
+    
+    # Appraoch 2: Storing <index, value> pairs, slower approach
+#     def __init__(self, nums: List[int]):
+#       # Add only values that aren't 0 storing their index as well    
+#         self.indexvalue = []
+#         for i, v in enumerate(nums):
+#             if v != 0:
+#                 self.indexvalue.append([i, v])
+                
+#     # Return the dotProduct of two sparse vectors
+#     def dotProduct(self, vec: 'SparseVector') -> int:
+#         product = 0
+#         i = 0
+#         j = 0
+#         while i < len(self.indexvalue) and j < len(vec.indexvalue):
+
+#             # They have the same index
+#             if (self.indexvalue[i][0] == vec.indexvalue[j][0]):
+#                 product += self.indexvalue[i][1] * vec.indexvalue[j][1]
+#                 i += 1
+#                 j += 1
+#           # Index of i < index of j, increase i
+#             elif (self.indexvalue[i][0] < vec.indexvalue[j][0]):
+#                 i += 1
+#             else:
+#                 j += 1
+            
+#         return product
+            
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
