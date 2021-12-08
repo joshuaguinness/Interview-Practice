c29c7f4f181a45f134e29b86624b9cb9e52ae761
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-08 18:19:31 -0500

    Time: 57 ms (25.79%), Space: 14.2 MB (85.45%) - LeetHub

--- add-strings/add-strings.py
+++ add-strings/add-strings.py
@@ -1,10 +1,10 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        # Dictionary of string to values
-        # Iterate backwards through string
-        # add the two numbers
         
-        # my_dict = {"0": 0, ""}
+        # Iterate through both strings backwards
+        # Add each individual number together and add it to a list
+        # Don't forget to include the carry
+        # Convert list to string and return
         
         i, j = len(num1)-1, len(num2)-1
         carry = 0
@@ -14,12 +14,14 @@ class Solution:
             num1_val = 0 if i < 0 else int(num1[i]) # python ternary operator
             num2_val = 0 if j < 0 else int(num2[j])
             unit_res = num1_val + num2_val + carry
+            # Nifty carry logic
             carry, unit_res = divmod(unit_res, 10)
             res.append(unit_res)
             i -= 1
             j -= 1
         
         res_str = ""
+        # Don't forget to reverse since we went through backwards
         for val in res[::-1]:
             res_str += str(val)
         return res_str
