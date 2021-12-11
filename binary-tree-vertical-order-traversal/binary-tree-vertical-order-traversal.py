53a80920fffe17c2a3b19c2e940e392a04957351
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-11 12:44:11 -0500

    Time: 36 ms (62.68%), Space: 14.2 MB (88.48%) - LeetHub

--- binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,22 +7,48 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        # BFS
+        # BFS w/ Sorting
+        # if root is None:
+        #     return []
+#         columnTable = defaultdict(list)
+#         queue = deque([(root, 0)])
+        
+#         while queue:
+#             node, col = queue.popleft()
+            
+#             if node:
+#                 columnTable[col].append(node.val)
+                
+#                 queue.append((node.left, col-1))
+#                 queue.append((node.right, col+1))
+            
+#         # newList = []
+#         # for i in sorted(columnTable):
+#         #     newList.append(columnTable[i])
+#         # return newList
+#         return [columnTable[x] for x in sorted(columnTable.keys())]
+
+        # BFS w/o Sorting
+        if root is None:
+            return []
         columnTable = defaultdict(list)
         queue = deque([(root, 0)])
+        min_col = 0
+        max_col = 0
         
         while queue:
             node, col = queue.popleft()
             
             if node:
+                min_col = min(min_col, col)
+                max_col = max(max_col, col)
+                
                 columnTable[col].append(node.val)
                 
                 queue.append((node.left, col-1))
                 queue.append((node.right, col+1))
-            
-        newList = []
-        for i in sorted(columnTable):
-            newList.append(columnTable[i])
-        return newList
-        # return [columnTable[x] for x in sorted(columnTable.keys())]
-        
+         
+        new_list = []
+        for i in range(min_col, max_col+1):
+            new_list.append(columnTable[i])
+        return new_list
