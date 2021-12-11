a77c4643f99aa2314589dbf41201cd4bef2c102b
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-11 14:19:16 -0500

    Time: 260 ms (59.72%), Space: 16.7 MB (85.43%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,11 +1,10 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        # Using HashMap
+        # Using HashMap, O(n)
         my_map = {0: 1}
         cumm_sum = 0
         count = 0
-        
         for i, v in enumerate(nums):
             cumm_sum += nums[i]
             if (cumm_sum - k in my_map.keys()):
@@ -14,7 +13,15 @@ class Solution:
                 my_map[cumm_sum] += 1
             else:
                 my_map[cumm_sum] = 1
-        
-            
-             
         return count
+
+        # Using Cummulative Sum, O(n^2)
+#         count = 0
+#         cumm_sum = [0]
+#         for i in range(1,len(nums)+1):
+#             cumm_sum.append(cumm_sum[i-1] + nums[i-1])
+#         for i in range(0, len(nums)+1):
+#             for j in range(i+1,len(nums)+1):
+#                 if (cumm_sum[j] - cumm_sum[i] == k):
+#                     count += 1   
+#         return count
