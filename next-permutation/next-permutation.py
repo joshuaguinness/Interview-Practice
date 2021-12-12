c6ddadc39f2458dc31216a057b4af43688530dda
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-12 10:07:52 -0500

    Time: 44 ms (57.99%), Space: 14.3 MB (54.05%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -4,22 +4,28 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
+        # Iterate from the back and find first element that is smaller than the previous
         i = len(nums)-2
         while i >= 0:
             if nums[i+1] > nums[i]:
                 break
             i-=1
             
-        print(i)
+        # Assuming i is not -1, iterate from the back and find the first element
+        # that is larger than nums[i], this will be the value that the two
+        # will be swapped to get the next largers
         if (i >= 0):
             j = len(nums)-1
             while nums[j] <= nums[i]:
                 j-=1
             nums[i], nums[j] = nums[j], nums[i]
 
+        # Reverse the remaining i+1 elements in the array so that it really is
+        # the next largest permutation
+        # If the array is in descending order, this will also reverse it to be
+        # in ascending order
         start = i+1
         end = len(nums)-1
-
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
