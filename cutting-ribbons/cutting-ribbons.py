dba44faac3c41ff67a264df3203bc519b634c810
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-13 11:28:54 -0500

    Time: 1692 ms (59.25%), Space: 28.7 MB (45.30%) - LeetHub

--- cutting-ribbons/cutting-ribbons.py
+++ cutting-ribbons/cutting-ribbons.py
@@ -5,11 +5,7 @@ class Solution:
         if k > sum(ribbons):
             return 0
         
-        # k is the same length as ribbons, therefore just return minimum of ribbons
-        # if len(ribbons) >= k:
-        #     return min(ribbons)
-        
-        # Binary search on space [1 min(ribbons)] to find the maximum length
+        # Binary search on space [1 max(ribbons)] to find the maximum length
         low = 1
         high = max(ribbons)
         
@@ -17,7 +13,6 @@ class Solution:
             mid = (low + high + 1) // 2
             
             sum_num_ribbons = 0
-            
             for ribbon in ribbons:
                 sum_num_ribbons += ribbon // mid
                 
@@ -25,30 +20,5 @@ class Solution:
                 low = mid
             else:
                 high = mid - 1
-            
-            # if sum_num_ribbons < k:
-            #     high = mid-1   
-            # else:
-                # low = mid
                 
         return low
-    
-    
-    
-#     class Solution:
-#     def maxLength(self, ribbons: List[int], k: int) -> int:
-#         total = sum(ribbons)
-#         if k > total:
-#             return 0
-        
-#         lo, hi = 1, min(total // k, max(ribbons))
-        
-#         while lo < hi:
-#             mid = (lo + hi + 1) // 2
-#             if sum(x // mid for x in ribbons) >= k:
-#                 lo = mid
-#             else:
-#                 hi = mid - 1
-        
-    
-    
