fad3d5b82cf41d3f813908b457b30a89b8d9bd1c
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-28 13:04:39 -0500

    Time: 76 ms (97.42%), Space: 16.2 MB (54.48%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -3,22 +3,31 @@ class Solution:
         
         # Sort the intervals on the first element
         intervals.sort(key=lambda item: item[0])
-        print(intervals)
         
+        n = len(intervals)
         new_intervals = []
         
         i = 0
-        while i < len(intervals):
+        # Iterate through the intervals array
+        while i < n:
+            
+            # Current interval to add to the array is start and end
+            # of current interval
             start = intervals[i][0]
-            end = intervals[i][1]
-            while i < len(intervals)-1 and end >= intervals[i+1][0]:
+            end = intervals[i][1] 
+            
+            # If the end of the current element is larger than the
+            # start of the next element, and its not at the end of
+            # the array, there is an overlap
+            
+            # Modify the end to be the max of the current
+            # end and the end of the next interval
+            while i < n-1 and end >= intervals[i+1][0]:
                 end = max(intervals[i+1][1], end)
                 i += 1
                 
+            # Add new interval to array
             new_intervals.append([start, end])
             i += 1
             
         return new_intervals
-                
-            
-                
