ce4af6403436bbe6966c4c8026a4e365868646f8
Joshua Guinnness <joshua.guinness@gmail.com>
2021-12-30 13:10:45 -0500

    Time: 32 ms (72.31%), Space: 14.2 MB (72.77%) - LeetHub

--- simplify-path/simplify-path.py
+++ simplify-path/simplify-path.py
@@ -6,26 +6,19 @@ class Solution:
         
         path_list = path.split('/')
         recent = ''
-        print(path_list)
         
         for i, v in enumerate(path_list):
             if v == '':
                 continue
-                # if recent != '/':
-                #     queue.append('/')
+            elif v == '.':
+                continue
             elif v == '..':
                 if len(queue) > 1:
                     queue.pop()
                     queue.pop()
-            elif v == '.':
-                continue
             else:
                 queue.append(v)
                 queue.append('/')
-                
-            if queue:
-                recent = queue[-1]
-            print(queue)
                 
         if len(queue) > 1 and queue[-1] == '/':
             queue.pop()
