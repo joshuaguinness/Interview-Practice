360bef69c30e7287ba942dd564b2ed452d528cc6
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-01 16:17:17 -0500

    Time: 32 ms (87.41%), Space: 14.4 MB (47.29%) - LeetHub

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -3,25 +3,38 @@ class Solution:
         
         my_dict = {}
         
+        # Compare word1 and word2 to see if correct order
         def compare(word1, word2):
-            in_order = True
+            
+            # Enumerate through the first word
             for i, char in enumerate(word1):
+                # If word2 is shorter than word1
+                # We know it is out of order and word2
+                # should go before word1
                 if i >= len(word2):
                     return False
+                # Two characters are in correct order
+                # Therefore words are in correct order
                 elif my_dict[char] < my_dict[word2[i]]:
                     break
+                # Two characters are the same
                 elif my_dict[char] == my_dict[word2[i]]:
                     continue
+                # Characters are out of order, therefore words are too
                 else:
                     return False
             return True
         
         # Add the alphabet order to a dictionary
+        # <key, value> = <letter, order>
         i = 1
         for char in order:
             my_dict[char] = i
             i += 1
         
+        # Loop through the list and compare adjacent words
+        # By verifying adjacent words are in correct order, we verify
+        # that the entire order is correct
         j = 0
         while j < len(words) - 1:
             # Compare jth word and jth+1 word
@@ -30,4 +43,3 @@ class Solution:
             j += 1
             
         return True
-            
