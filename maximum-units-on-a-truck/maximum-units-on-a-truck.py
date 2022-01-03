98a031c29d8235eef2704dccc87888567954e9a0
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-02 21:50:22 -0500

    Time: 236 ms (20.62%), Space: 14.6 MB (91.00%) - LeetHub

--- maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -2,26 +2,26 @@ class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         
         # Fill up the truck in order of largest
-        # number of units per box
+        # number of units per box. Greedy algorithm,
+        # will always result in max number of units on
+        # the truck
         
         # Sort by units / box
         boxTypes.sort(key = lambda x:x[1], reverse=True)
-        # print(boxTypes)
         
         output = 0
         
-        i = 0
-        # while truckSize > 0:
-        #     # if truckSize - boxTypes[i][0] >= 0:
-        #     #     output += boxTypes[i][0] * boxTypes[i][1]
-        #     num_boxes = min(truckSize, boxTypes[i][0])
-        #     output += num_boxes * boxTypes[i][1]
-        #     truckSize -= num_boxes
-        #     i += 1
         for i, v in enumerate(boxTypes):
+            
+            # No more space on the truck
             if truckSize == 0:
                 break
+                
+            # Number of boxes to add to the truck of
+            # this box type
             num_boxes = min(truckSize, boxTypes[i][0])
+            
             output += num_boxes * boxTypes[i][1]
             truckSize -= num_boxes
+            
         return output
