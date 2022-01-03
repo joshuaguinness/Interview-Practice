a92d72195bb0f98499a339f6fa8fcb6bd26d36f9
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-02 21:05:15 -0500

    Time: 190 ms (5.17%), Space: 14.4 MB (95.80%) - LeetHub

--- leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.py
+++ leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.py
@@ -9,6 +9,7 @@
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
         
+        # Binary search to find the leftmost 1
         def binarySearch(row, lo, hi):
             while lo < hi:
                 mid = (lo + hi) // 2
@@ -22,20 +23,25 @@ class Solution:
             return lo
         
         rows, cols = binaryMatrix.dimensions()
+        
         # Initialize leftmost column with the number of columns
         leftmost = cols
         
         # Iterate through each row
         for i in range(rows):
+            
             # Last element in row is zero means
             # that there is no 1 in the entire row,
             # move to the next row
             if binaryMatrix.get(i, cols-1) == 0:
                 continue
+                
             # There is a 1 in this row, binary search
             # to find it
             else:
                 index = binarySearch(i, 0, cols-1)
+                
+                # Keep track of current minimum only
                 leftmost = min(leftmost, index)
         
         # No 1's
@@ -43,8 +49,3 @@ class Solution:
             return -1
         else:
             return leftmost
-        
-        
-# 0 0 1 1 1
-
-# lo = 0, hi = 
