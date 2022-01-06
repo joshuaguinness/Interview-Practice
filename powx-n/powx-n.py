0ddd56660999bf5c8fce27fef4670e59c9d5ada6
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-06 17:41:01 -0500

    Time: 38 ms, Memory: 14.1 MB - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,6 +1,8 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
+        # x^n = x^(n/2) * x^(n/2)
+        # Therefore, only do half the work each recursive call
         def Pow(x, n):
             if n == 0:
                 return 1
@@ -10,9 +12,12 @@ class Solution:
             else:
                 return half * half * x
         
+        # If n is negative, ensure n is positive and x is 1/x
+        # This is how negative powers work
         if n < 0:
             n = abs(n)
             x = 1 / x
+            
         return Pow(x, n)
         
         # Slow O(n), iterative solution
