0027e3d2cbe0ee5a91e48651849159cd3d5e9d9b
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-06 21:43:21 -0500

    Time: 31 ms (59.28%), Space: 14.4 MB (24.49%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.py
+++ 163-missing-ranges/163-missing-ranges.py
@@ -3,31 +3,29 @@ class Solution:
         
         missing = []
     
+        # nums doesn't exist
+        # Append to missing array depending on whether 
+        # upper and lower equal each other
         if not nums:
-            if lower == upper:
-                missing.append(str(lower))
-            else:
-                missing.append(str(lower) + "->" + str(upper))
+            if lower == upper: missing.append(str(lower))
+            else: missing.append(str(lower) + "->" + str(upper))
             return missing
             
-        if nums[0] - lower > 1:
-            missing.append(str(lower) + "->" + str(nums[0]-1))
-        elif nums[0] - lower == 1:
-            missing.append(str(lower))
+        # Adding possible missing ranges before first element
+        if nums[0] - lower > 1: missing.append(str(lower) + "->" + str(nums[0]-1))
+        elif nums[0] - lower == 1: missing.append(str(lower))
     
         i = 0
+        # Iterate through and add any possible missing ranges between elements
         while i < len(nums)-1 and len(nums) > 1:
-            if nums[i+1] - nums[i] > 1:
-                if nums[i+1] - nums[i] > 2:
-                    missing.append(str(nums[i]+1) + "->" + str(nums[i+1]-1))
-                else:
-                    missing.append(str(nums[i] + (nums[i+1] - nums[i] - 1)))
+            if nums[i+1] - nums[i] > 2:
+                missing.append(str(nums[i]+1) + "->" + str(nums[i+1]-1))
+            elif nums[i+1] - nums[i] == 2:
+                missing.append(str(nums[i] + (nums[i+1] - nums[i] - 1)))
             i += 1
             
-        if upper - nums[-1] > 0:
-            if upper - nums[-1] > 1:
-                missing.append(str(nums[-1]+1) + "->" + str(upper))
-            else:
-                missing.append(str(upper))
+        # Adding possible missing ranges after last element
+        if upper - nums[-1] > 1: missing.append(str(nums[-1]+1) + "->" + str(upper))
+        elif upper - nums[-1] == 1: missing.append(str(upper))
         
         return missing
