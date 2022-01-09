0a599504fd47371622dec2107979a18c6cca4092
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-09 10:26:18 -0500

    Time: 30 ms (63.18%), Space: 14.2 MB (82.34%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -5,48 +5,47 @@ class Solution:
         # first number that is larger than the prev
         # aka iterate until no longer decreasing
         # Then find the max of remaining numbers
-        # Then insert that max as high as possible in
-        # the decreasing numbers first part
+        # Then insert that max number as high as possible in
+        # the first part of the number
         
+        # Convert num to a list of string digits
         nums = list(map(str, str(num)))
         local_max = -1
         max_index = -1
         it_start = -1
+        
+        # Iterate through the nums array
         for i, v in enumerate(nums):
+            
+            # Numbers are no longer decreasing
             if i < len(nums)-1 and v < nums[i+1]:
+                
+                # Index to start iterating at
+                # when searching for value to swap
                 it_start = i
+                
                 # Find the max of the rest of the array
                 # iterate backwards so it finds the last max
                 for j in range(len(nums)-1, i, -1):
                     if int(nums[j]) > int(local_max):
                         local_max = nums[j]
                         max_index = j
-                # local_max = max(nums[i+1:])
-                # max_index = nums[i+1:].index(local_max) + (i + 1)
                 break
-        
-        print(max_index, local_max)
-        print(it_start)
         
         # num is already the max, no swaps can
         # make it higher
         if local_max == -1:
             return num
         
-        # i = max_index
+        # Iterate from the index where the digits
+        # Stoped decreasing to the beginning to find
+        # the element that the max should be swapped with
         i = it_start
         while i > 0 and local_max > nums[i-1]:
             i -= 1
-        # Iterate from other side
         
-        # while i < len(nums)-1 and local_max > nums[i]
-        
-        print(i)
-        # if i != 0:
-        #     i += 1
+        # Swap the numbers
         nums[i], nums[max_index] = nums[max_index], nums[i]
         
+        # Return
         return int("".join(nums))
-            
-  # 32176
-  # 73216
