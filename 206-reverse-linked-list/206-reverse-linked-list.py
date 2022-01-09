23f483c4f00c6197b7410e14cf1717c33f9cd4e6
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-09 09:03:12 -0500

    Time: 59 ms (12.18%), Space: 19.2 MB (7.47%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -7,12 +7,25 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         # Iterative
-        prev = None
-        curr = head
-        while curr != None:
-            next_temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_temp
-            
+        # prev = None
+        # curr = head
+        # while curr != None:
+        #     next_temp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = next_temp
+        # return prev
+    
+        # Recursive
+        if (head == None) or (head.next == None):
+            return head
+        
+        prev = self.reverseList(head.next)
+        # Reverse part
+        head.next.next = head
+        # Pointing to None
+        # If its the last element, then it will point
+        # to None and be good
+        head.next = None
+        # This will be the new head/start of LL
         return prev
