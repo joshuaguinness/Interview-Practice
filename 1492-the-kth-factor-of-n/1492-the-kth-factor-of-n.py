a6d99d9cc9b63fa0b5f25df50ea053fde5cad6eb
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-09 23:31:05 -0500

    Time: 32 ms (76.03%), Space: 14.4 MB (13.93%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,13 +1,29 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         
-        factors = []
+        # O(n) solution, simple and works but can be done better
+        # factors = []
+        # for i in range(1, n+1):
+        #     if n % i == 0:
+        #         factors.append(i)
+        # if k > len(factors):
+        #     return -1
+        # else:
+        #     return factors[k-1]
         
-        for i in range(1, n+1):
+        # Heap Solution
+        heap = []
+        # Iterate up to the square root (covers all values)
+        for i in range(1, int(n**0.5) + 1):
+            
+            # Divisible with no remainder add
             if n % i == 0:
-                factors.append(i)
-                
-        if k > len(factors):
+                heapq.heappush(heap, i)
+                # Ensure don't add same value twice
+                if i != n // i:
+                    heapq.heappush(heap, n//i)
+         
+        if k > len(heap):
             return -1
         else:
-            return factors[k-1]
+            return heapq.nsmallest(k, heap)[-1]
