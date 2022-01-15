0667c431cf320d49537ec7d696c217cded6d4f80
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-15 17:49:25 -0500

    Time: 48 ms (21.68%), Space: 14.3 MB (54.87%) - LeetHub

--- 68-text-justification/68-text-justification.py
+++ 68-text-justification/68-text-justification.py
@@ -9,7 +9,6 @@ class Solution:
         i = 0
         while i < len(words):
             word = words[i]
-            print(l_remaining, curr_line)
             
             # There is room on the line for the word
             if len(word) <= l_remaining:
@@ -21,29 +20,33 @@ class Solution:
             
             # There is no space left on the line for the word, pack with spaces
             else:
-                
                 # Add back spaces taken off for calculation purposes
                 l_remaining += len(curr_line)
+                
+                # Only one word on that line
                 if len(curr_line) == 1:
+                    # Add to output with extra spaces on end
                     output.append(curr_line[0] + " "*l_remaining)
+                    # Reset variables
                     l_remaining = maxWidth
                     curr_line = []
                     continue
                 
-                # Spaces to add between each word
+                # Calculate the spaces to add between each word
+                # and add them in the loop
                 num_spaces = l_remaining // (len(curr_line)-1)
-                
                 for j in range(len(curr_line)-1):
                     curr_line[j] += " "*num_spaces
                     
-                # Extra spaces to add starting from the left
+                # Calculate any extra spaces to add starting from the left
                 num_spaces_2 = l_remaining % (len(curr_line)-1)
-                
                 for j in range(num_spaces_2):
                     curr_line[j] += " "
                     
+                # Add to output the justified line
                 output.append("".join(curr_line))
                 
+                # Reset variables
                 l_remaining = maxWidth
                 curr_line = []
                 
