f512296d42a507a7b43c08a4c197c64137ac1637
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-16 20:56:40 -0500

    Time: 28 ms (85.44%), Space: 14.2 MB (55.31%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -9,12 +9,14 @@ class Solution:
         
         for i, v in enumerate(pattern):
             
+            string_val = split_s[i]
+            
             if v in my_dict:
-                if my_dict[v] != split_s[i]:
+                if my_dict[v] != string_val:
                     return False
             else:
-                if split_s[i] not in my_dict.values():
-                    my_dict[v] = split_s[i]
+                if string_val not in my_dict.values():
+                    my_dict[v] = string_val
                 else:
                     return False
                 
