2577dc66b8b60c491d9c418d47d46833301a9c15
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-17 12:54:42 -0500

    Time: 27 ms (35.13%), Space: 6.4 MB (95.53%) - LeetHub

--- 268-missing-number/268-missing-number.c
+++ 268-missing-number/268-missing-number.c
@@ -2,17 +2,32 @@
 
 int missingNumber(int* nums, int numsSize){
     
-    int missing_value = numsSize;
+//     int missing_value = numsSize;
 
-    // XOR integer n with ever index and value
-    // to find the missing number. See solution #3
-    // for more details.
-    for (int i = 0; i < numsSize; i++){
+//     // XOR integer n with ever index and value
+//     // to find the missing number. See solution #3
+//     // for more details.
+//     for (int i = 0; i < numsSize; i++){
         
-        // XOR = True if exactly one, but not both conditions are true
-        missing_value ^= i ^ nums[i];
+//         // XOR = True if exactly one, but not both conditions are true
+//         missing_value ^= i ^ nums[i];
 
+//     }
+//     return missing_value;
+    
+    
+    // Gauss Approach
+    
+    // Gets the sum of numbers 1 -> n
+    int expectedSum = numsSize * (numsSize + 1) / 2;
+    int actualSum = 0;
+    
+    // Calculate the actual sum of the numbers in the array
+    for (int i = 0; i < numsSize; i++){
+        actualSum += nums[i];
     }
-    return missing_value;
+    
+    // Return the difference which is number missing
+    return expectedSum - actualSum;
     
 }
