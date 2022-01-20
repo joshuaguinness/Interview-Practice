f4f43beff971798d8df7f61a5d983e6eca298555
Joshua Guinnness <joshua.guinness@gmail.com>
2022-01-19 22:08:23 -0500

    Time: 1144 ms (18.25%), Space: 20.4 MB (20.42%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,32 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        # Iterate through the array
+        # Add to a new array the distance
+        # Sort the array based on distance
+        # Return the first element of the array k times, popping the first element each time
+        
+#         distances = []
+        
+#         for i, v in enumerate(points):
+#             distances.append((i, v[0]**2 + v[1]**2))
+            
+#         distances.sort(key=lambda x:x[1])
+#         min_distances = []
+#         for i in range(k):
+#             min_distances.append(points[distances[0][0]])
+#             distances.pop(0)
+            
+#         return min_distances
+
+
+        # Using a PQ and list comprehension
+        # Create a PQ for all the points
+        distances = [(v[0]**2 + v[1]**2, i) for i, v in enumerate(points)]
+        heapq.heapify(distances)
+
+        # Get the k shortest distances
+        shortest = heapq.nsmallest(k, distances)
+
+        # Return the points as a list
+        return [points[i[1]] for i in shortest]
